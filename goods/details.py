19e2e728dd7e133e355b154c22bc04a2a07bf08a
Piyuyang <pi_yuyang@163.com>
2019-09-12 19:54:41 +0800

    restful-响应-序列化数据

--- goods/details.py
+++ goods/details.py
@@ -1,4 +1,5 @@
-from flask_restful import Resource
+from flask_restful import Resource, fields, marshal, marshal_with
+
 
 # from . import goods_bp
 
@@ -8,6 +9,31 @@ from flask_restful import Resource
 #     return 'details'
 
 
+class Goods(object):
+    """
+    模拟返回的数据对象的类
+    """
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+resource_fields = {
+    'name': fields.String,
+    'price': fields.Float
+}
+
+
 class DetailsResource(Resource):
+
+    # 方式一：装饰器
+    # @marshal_with(resource_fields, envelope='data')
+    # def get(self):
+    #     goods = Goods('iphone', 8000.00)
+    #     return goods
+
+    # 方式二：函数
     def get(self):
-        return {"msg": "in DetailsResource"}
+        goods = Goods('iphone', 8000.00)
+        return marshal(goods, resource_fields, envelope='data')
